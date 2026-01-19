8dacfe54f763253495c0159b2b09e06ed29b99f0
PRATHAMS009 <prathams@circulants.com>
2026-01-19 06:16:55 +0000

    .

---
+++ target/compiled/dbt_full_project_tutorial/analyses/StoreRevenue.sql
@@ -0,0 +1,8 @@
+SELECT
+    OS.StoreID,
+    SUM(OFACT.Revenue) AS TotalRevenue
+FROM
+    dbt_project_db.dbt_project_schema_l3_dbt_project_schema_l2.orders_stg OS
+JOIN
+    dbt_project_db.dbt_project_schema_l3_dbt_project_schema_l2.orders_fact OFACT ON OS.OrderID = OFACT.OrderID
+GROUP BY 1
